0acffe2ba60b522611e9ce6fd639f53046ab7c5b
GabiiF <gabriela.fernandez.92@live.com.ar>
2025-12-03 03:27:50 -0300

    Arreglo orden

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -39,8 +39,8 @@ const Login = () => {
                             <label htmlFor='usuario' className="block text-sm/6 font-medium text-gray-900">Usuario</label>
                             <div className="mt-2">
                                 <input
-                                    type='text'
                                     id="usuario"
+                                    type='text'
                                     name="usuario"
                                     value={usuario}
                                     onChange={(evento) => setUsuario(evento.target.value)}
@@ -69,8 +69,8 @@ const Login = () => {
                                     name="password"
                                     value={contrasenia}
                                     onChange={(evento) => setContrasenia(evento.target.value)}
-                                    required
                                     autoComplete="current-password"
+                                    required
                                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 />
                             </div>
