f2429411c039cda19b166dacac9f24aa86d1957e
GabiiF <gabriela.fernandez.92@live.com.ar>
2025-12-10 09:07:27 -0300

    arreglo problema de key en Carrito.jsx

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -3,11 +3,11 @@ import { CarritoContext } from "../context/CarritoContext";
 import { Link } from "react-router-dom";
 
 const Carrito = () => {
-    //llamamos del context
-    const {carrito, eliminarDelCarrito} =useContext(CarritoContext);
+  //llamamos del context
+  const { carrito, eliminarDelCarrito } = useContext(CarritoContext);
 
-    {/*Si no hay produsctos en el carrito */}
-    if (carrito.length === 0) {
+  {/*Si no hay produsctos en el carrito */ }
+  if (carrito.length === 0) {
     return (
       <div className="min-h-[60vh] flex flex-col items-center justify-center px-4">
         <div className="text-center">
@@ -24,25 +24,25 @@ const Carrito = () => {
     );
   }
 
-    return (
-        <div className="space-y-4">
-            <h2 className="text-3xl font-bold mb-8">Carrito</h2>
-            {carrito.map((producto, indice) =>
-            <div className="bg-white border border-gray-200 rounded-lg p-4 sm:p-6">
-                <div key={indice} className="grid grid-cols-3 gap-4 mb-6">
-                    <img src={producto.image} alt={producto.title} width={80} height={80}></img>
-                    <span className="col-span-2">
-                        <h3>{producto.title}: ${producto.price}</h3>
-                        <button onClick={() => eliminarDelCarrito(indice)}
-                            className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-red-500 active:bg-red-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                            Eliminar</button>
-                    </span>
-                </div>
-            </div>
-            )}
-
+  return (
+    <div className="space-y-4">
+      <h2 className="text-3xl font-bold mb-8">Carrito</h2>
+      {carrito.map((producto, indice) =>
+        <div key={indice} className="bg-white border border-gray-200 rounded-lg p-4 sm:p-6">
+          <div className="grid grid-cols-3 gap-4 mb-6">
+            <img src={producto.image} alt={producto.title} width={80} height={80}></img>
+            <span className="col-span-2">
+              <h3>{producto.title}: ${producto.price}</h3>
+              <button onClick={() => eliminarDelCarrito(indice)}
+                className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-red-500 active:bg-red-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Eliminar</button>
+            </span>
+          </div>
         </div>
-    );
+      )}
+
+    </div>
+  );
 
 }
 export default Carrito;
