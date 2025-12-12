b6e0194a7aeeb84b26b224ba958a9223ce43520a
GabiiF <gabriela.fernandez.92@live.com.ar>
2025-12-12 01:00:07 -0300

    arreglo error por class

--- src/pages/Tecnologia.jsx
+++ src/pages/Tecnologia.jsx
@@ -1,9 +1,8 @@
 const Tecnologia = () => {
 
     return (
-        <div class="px-4 sm:px-6 lg:px-8 py-8">
+        <div className="px-4 sm:px-6 lg:px-8 py-8">
             <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl"> Tecnología </h1>
-
         </div>
     );
 }
