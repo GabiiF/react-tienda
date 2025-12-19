b41b65c784c5e9417913ba3971eeb410995edb5b
GabiiF <gabriela.fernandez.92@live.com.ar>
2025-12-19 01:03:18 -0300

    Cambie logo de login y margin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -37,10 +37,10 @@ const Login = () => {
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <img
                         alt="Your Company"
-                        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                        className="mx-auto h-10 w-auto"
+                        src="../ventaOnline.png"
+                        className="mx-auto h-35 w-auto"
                     />
-                    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Ingresa tu cuenta</h2>
+                    <h2 className="mt-8 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Ingresa tu cuenta</h2>
                 </div>
                 {/*Usuario*/}
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
